7eba240695e2908372e366be7d3029cf19b4ab15
syarifhsb <syarifhsb.dev@gmail.com>
2026-05-14 21:17:58 +0200

    feat: resize footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,19 @@ export default function Footer() {
     <footer className="flex flex-row items-center justify-between px-6 py-4 border-t border-border">
       <div className="flex flex-row gap-4">
         <a className="icon-white opacity-60 hover:opacity-100 transition-opacity" href="mailto:syarifhsb.dev@gmail.com" target="_blank" rel="noreferrer">
-          <img src="/assets/icons/mail.svg" alt="Email" className="w-4 h-4" />
+          <img src="/assets/icons/mail.svg" alt="Email" className="w-5 h-5" />
         </a>
         <a className="icon-white opacity-60 hover:opacity-100 transition-opacity" href="https://github.com/syarifhsb" target="_blank" rel="noreferrer">
-          <img src="/assets/icons/github.svg" alt="GitHub" className="w-4 h-4" />
+          <img src="/assets/icons/github.svg" alt="GitHub" className="w-5 h-5" />
         </a>
         <a className="icon-white opacity-60 hover:opacity-100 transition-opacity" href="https://www.linkedin.com/in/syarifhasibuan/" target="_blank" rel="noreferrer">
-          <img src="/assets/icons/linkedin.svg" alt="LinkedIn" className="w-4 h-4" />
+          <img src="/assets/icons/linkedin.svg" alt="LinkedIn" className="w-5 h-5" />
         </a>
         <a className="icon-white opacity-60 hover:opacity-100 transition-opacity" href="https://x.com/syarif_hsb" target="_blank" rel="noreferrer">
-          <img src="/assets/icons/twitter.svg" alt="X" className="w-4 h-4" />
+          <img src="/assets/icons/twitter.svg" alt="X" className="w-5 h-5" />
         </a>
       </div>
-      <p className="text-muted text-xs">&copy; 2025 Syarif Hasibuan</p>
+      <p className="text-muted text-sm">&copy; 2026 Syarif Hasibuan</p>
     </footer>
   )
 }
